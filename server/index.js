fd92f485e150d0bdc07518a2b3650a8a87d7033c
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-23 15:16:24 -0300

    Colocando servidor para rodar

--- server/index.js
+++ server/index.js
@@ -1,3 +1,15 @@
+const { error } = require("console");
+
 const app = require("express")()
 const server =  require("http").createServer(app)
 const io =  require("socket.io")(server, {cors: {origin: 'http://localhost:5173/ '}})
+
+const PORT = 8081;
+
+server.listen(PORT, (error)=>{
+    if(error){
+        console.log(error)
+    }
+
+    console.log(`Servidor rodando na porta: ${PORT}`)
+})
