2bd4e3f95c33ffdb5d93082409bc7cdbe91182b0
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-25 20:58:51 -0300

    criando ref para receber o valor do input e mudar estado do chat caso haja valor

--- client/src/Components/Join/Join.jsx
+++ client/src/Components/Join/Join.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { useRef } from "react";
+const Join = ( {setChatVisibility}) =>{
 
-const Join = () =>{
+    const usernameRef  = useRef()
+    
+    const handleClickEntrarButton = () =>{
+        const username = usernameRef.current.value
+        if(!username.trim()) return
+        
+        setChatVisibility(true)
+    }
+
     return(
         <>
             <h1>Join</h1>
-            <input type="text" placeholder="Nome de usuário" />
-            <button>Entrar</button>
+            <input type="text" ref={usernameRef} placeholder="Nome de usuário" />
+            <button onClick={handleClickEntrarButton}>Entrar</button>
         </>
     )
 }
