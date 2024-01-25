d8ab427e279a4dd32ca3cc46b2e0a529b3057f9b
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-25 20:53:39 -0300

    passando o state como props no join

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
     {
-        chatVisibility ? <Chat /> : <Join />
+        chatVisibility ? <Chat /> : <Join setChatVisibility={setChatVisibility}/>
     }
     </>
   )
