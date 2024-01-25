14bd50876a2f32b642bb359a6b9efb542085b0f3
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-25 20:44:46 -0300

    Criando componente com input e btn de enviar mensagem

---
+++ client/src/Components/Chat/Chat.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Chat = () =>{
+    return(
+        <>
+            <h1>Chat</h1>
+            <input type="text" placeholder="Mensagem" />
+            <button>Enviar</button>
+        </>
+    )
+}
+
+export default Chat
