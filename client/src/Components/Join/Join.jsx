bfce162ab5066b28ccb0686474785de97c121d32
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-25 20:44:31 -0300

    adicionando input e btn

--- client/src/Components/Join/Join.jsx
+++ client/src/Components/Join/Join.jsx
@@ -3,6 +3,9 @@ import React from "react";
 const Join = () =>{
     return(
         <>
+            <h1>Join</h1>
+            <input type="text" placeholder="Nome de usuário" />
+            <button>Entrar</button>
         </>
     )
 }
