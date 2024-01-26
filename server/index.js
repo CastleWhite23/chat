dc52084a0531649db73c906a0a8b6dac27cdb302
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-25 21:06:11 -0300

    deixando o websocket on

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
-const { error } = require("console");
 
 const app = require("express")()
 const server =  require("http").createServer(app)
 const io =  require("socket.io")(server, {cors: {origin: 'http://localhost:5173/ '}})
 
 const PORT = 8081;
+
+io.on('connection', socket => {
+    console.log('usuario conectado', socket.id)
+})
+
 
 server.listen(PORT, (error)=>{
     if(error){
