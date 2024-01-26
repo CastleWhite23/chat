e2b4e3dc67dce1a6852a44df6acbdbf6c28200b6
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-26 08:26:23 -0300

    Recebendo props  de socket

--- client/src/Components/Chat/Chat.jsx
+++ client/src/Components/Chat/Chat.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { useRef } from "react";
 
-const Chat = () =>{
-    return(
+const Chat = ({ socket }) => {
+
+    const messageRef = useRef()
+
+    const handleClickEnviarButton = () => {
+
+    }
+
+    return (
         <>
             <h1>Chat</h1>
-            <input type="text" placeholder="Mensagem" />
-            <button>Enviar</button>
+            <input type="text" ref={messageRef} placeholder="Mensagem" />
+            <button onClick={handleClickEnviarButton}>Enviar</button>
         </>
     )
 }
