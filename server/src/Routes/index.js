a042b70e47b27d09b22a6b934cf3200288c1abd9
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-27 08:57:36 -0300

    Excluindo pasta das rotas

--- server/src/Routes/index.js
+++
@@ -1,8 +0,0 @@
-const routeMensagem  = require('./routeMensagem')
-
-
-module.exports = (app, express) =>{
-    app.use(express.json())
-    app.use(express.urlencode({ extended: true}))
-    app.use(routeMensagem)
-}
