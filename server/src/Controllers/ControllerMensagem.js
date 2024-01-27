bde918f461b5ef18cffbc12fb634fff325ae2f0f
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-27 09:19:06 -0300

    usando o try_cathc no controller

--- server/src/Controllers/ControllerMensagem.js
+++ server/src/Controllers/ControllerMensagem.js
@@ -1,10 +1,18 @@
+const ModelMensagem = require("../Models/ModelMensagem")
+
 class ControllerMensagens{
     getAllMessages(){
 
     }
 
-    postMessages(message, author){
+    async postMessages(mensagem, autor){
+        try{
+            const mensagenInserida =  await ModelMensagem.postMessages(mensagem, autor);
+        }catch{
+            console.log('deu ruim tropa da alt f4')
+        }
         
+    
     }
 }
 
