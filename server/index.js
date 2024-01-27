6609ddaeb0f8db7a2081ff55fec39d0019801698
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-01-27 08:59:32 -0300

    tirando comentarios

--- server/index.js
+++ server/index.js
@@ -1,8 +1,4 @@
 
-// const app = require("express")()
-// const server =  require("http").createServer(app)
-// const io =  require("socket.io")(server, )
-
 const express = require('express');
 const app = express();
 const http = require('http');
